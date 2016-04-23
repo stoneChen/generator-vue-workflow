e7610af2732810d5bcee00eb825338c61440ebe9
chenshidong <baby31529@gmail.com>
2016-04-23 21:02:23 +0800

    格式微调

--- app/templates/src/index.js
+++ app/templates/src/index.js
@@ -1,10 +1,10 @@
-<% if (props.isMobile) { %>import './utils/responsive' // 动态设置根字体
+<% if (props.isMobile) {
+%>import './utils/responsive'
 import FastClick from 'fastclick'
-FastClick.attach(document.body)// 引入fastclick，解决300ms延迟问题
-<% } %>
-import './config'
+FastClick.attach(document.body)
+<% }
+%>import './config'
 import App from 'views/app.vue'
-
 import router from './router'
 
 router.start(App, '#app')
