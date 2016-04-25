584d5ee8e3f0fe85b5a23b5931583c189372f5bd
chenshidong <baby31529@gmail.com>
2016-04-25 10:34:30 +0800

    proxy设置已经迁移到proxy.config.yml了,删掉

--- app/templates/global.config.js
+++ app/templates/global.config.js
@@ -3,11 +3,6 @@ module.exports = {
     //重要! 页面标题
     title: '页面标题' 
   },
-  //代理设置
-  // proxy: {
-  //   path: '/',
-  //   target: 'http://example.com'
-  // },
   //本地服务端口
   serverPort: 8000
 };
