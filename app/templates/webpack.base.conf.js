383261e8061131e52dc334840565009f67265cef
chenshidong <baby31529@gmail.com>
2016-05-18 12:18:23 +0800

    修复打包路径问题

--- app/templates/webpack.base.conf.js
+++ app/templates/webpack.base.conf.js
@@ -6,7 +6,7 @@ module.exports = {
     index: './src/index.js'
   },
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'dist/static'),
     filename: '[name].js'
   },
   resolve: {
